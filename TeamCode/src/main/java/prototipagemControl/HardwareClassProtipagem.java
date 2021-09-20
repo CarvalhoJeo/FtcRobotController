43842f9808fca937b6dbd342fca863f2d04aa611
lcarvalho14391 <carvgamer@gmail.com>
2021-09-20 13:46:54 -0300

    Start of the prototyping control class

---
+++ TeamCode/src/main/java/prototipagemControl/HardwareClassProtipagem.java
@@ -0,0 +1,122 @@
+package prototipagemControl;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class HardwareClassProtipagem {
+
+    /*
+     * =============================================================================
+     *                                MOTORES
+     * =============================================================================
+     */
+
+    public DcMotor motorEsquerda, motorDireita, motorEsquerdaTras, motorDireitaTras; //Motores do expansion de baixo
+    public DcMotor motorP0, motorP1, motorP2, motorP3; //Motores do expansion de cima
+
+    /*
+     * =============================================================================
+     *                                 SERVOS
+     * =============================================================================
+     */
+
+    public Servo servoP0,servoP1,servoP2,servoP3,servoP4,servoP5; //Servos do expansion de cima
+    //public CRServo servoP0,servoP1,servoP2,servoP3,servoP4,servoP5; //Servos Continuos do expansion de cima
+
+
+    //Referências de hardware e gyro
+    static BNO055IMU imu;
+    HardwareMap hwMap   =  null;
+
+    public void hardwareGeral(HardwareMap ahwMap) {
+        //Referência hardware
+        hwMap = ahwMap;
+
+        /*
+         * =============================================================================
+         *                                  GYRO
+         * =============================================================================
+         */
+
+        //Configura o gyro
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+
+        //Imu no Drive Station
+        imu = hwMap.get(BNO055IMU.class, "imu");
+
+        //Inicializa os parametros do gyro
+        imu.initialize(parameters);
+
+        /*
+         * =============================================================================
+         *                                  ACIONADORES
+         * =============================================================================
+         */
+
+        //Pega o nome das variáveis no Dv.
+        motorEsquerda = hwMap.get(DcMotor.class, "motor_Esquerda");
+        motorEsquerdaTras = hwMap.get(DcMotor.class, "motor_EsquerdaTras");
+        motorDireita = hwMap.get(DcMotor.class, "motor_Direita");
+        motorDireitaTras = hwMap.get(DcMotor.class, "motor_DireitaTras");
+        motorP0 = hwMap.get(DcMotor.class, "motor_P0");
+        motorP1 = hwMap.get(DcMotor.class, "motor_P1");
+        motorP2 = hwMap.get(DcMotor.class, "motor_P2");
+        motorP3 = hwMap.get(DcMotor.class, "motor_P3");
+        servoP0 = hwMap.get(Servo.class, "servo_P0");
+        servoP1 = hwMap.get(Servo.class, "servo_P1");
+        servoP2 = hwMap.get(Servo.class, "servo_P2");
+        servoP3 = hwMap.get(Servo.class, "servo_P3");
+        servoP4 = hwMap.get(Servo.class, "servo_P4");
+        servoP5 = hwMap.get(Servo.class, "servo_P5");
+        /*
+        servoP0 = hwMap.get(CRServo.class, "servo_P0");
+        servoP1 = hwMap.get(CRServo.class, "servo_P1");
+        servoP2 = hwMap.get(CRServo.class, "servo_P2");
+        servoP3 = hwMap.get(CRServo.class, "servo_P3");
+        servoP4 = hwMap.get(CRServo.class, "servo_P4");
+        servoP5 = hwMap.get(CRServo.class, "servo_P5");
+         */
+
+        //Direção dos motores
+        motorEsquerda.setDirection(DcMotor.Direction.REVERSE);
+        motorDireita.setDirection(DcMotor.Direction.FORWARD);
+        motorEsquerdaTras.setDirection(DcMotor.Direction.REVERSE);
+        motorDireitaTras.setDirection(DcMotor.Direction.FORWARD);
+        motorP0.setDirection(DcMotor.Direction.FORWARD);
+        motorP1.setDirection(DcMotor.Direction.FORWARD);
+        motorP2.setDirection(DcMotor.Direction.FORWARD);
+        motorP3.setDirection(DcMotor.Direction.FORWARD);
+        servoP0.setDirection(Servo.Direction.FORWARD);
+        servoP1.setDirection(Servo.Direction.FORWARD);
+        servoP2.setDirection(Servo.Direction.FORWARD);
+        servoP3.setDirection(Servo.Direction.FORWARD);
+        servoP4.setDirection(Servo.Direction.FORWARD);
+        servoP5.setDirection(Servo.Direction.FORWARD);
+        /*
+        servoP0.setDirection(CRServo.Direction.FORWARD);
+        servoP1.setDirection(CRServo.Direction.FORWARD);
+        servoP2.setDirection(CRServo.Direction.FORWARD);
+        servoP3.setDirection(CRServo.Direction.FORWARD);
+        servoP4.setDirection(CRServo.Direction.FORWARD);
+        servoP5.setDirection(CRServo.Direction.FORWARD);
+         */
+
+        //Reseta os encoder, por que são usados em dois OpModes
+        motorEsquerda.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorDireita.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorEsquerdaTras.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorDireitaTras.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //Coloca para se mexer contando o encoder
+        motorEsquerda.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorDireita.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorEsquerdaTras.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorDireitaTras.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+}
---
+++ TeamCode/src/main/java/prototipagemControl/TeleOpPrototipagem.java
@@ -0,0 +1,124 @@
+package prototipagemControl;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+@TeleOp(name="Teleoperado to test", group="Linear TesteOp")
+public class TeleOpPrototipagem extends LinearOpMode{
+
+    //Instanciação de objetos
+    ElapsedTime runtime = new ElapsedTime();
+    HardwareClassProtipagem hard = new HardwareClassProtipagem();
+
+    //Referência de oritenação do gyro
+    Orientation angles;
+
+    //Respectivamente eixos do gamepad y, x, x outro analógico
+    double drive, turn, giro;
+    double pi = 3.1415926;
+
+    //Vetor para potência do motor
+    private final double[] poder = new double[4];
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        //Inicia o hardware do robô
+        hard.hardwareGeral(hardwareMap);
+
+        hard.motorEsquerda.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        hard.motorDireita.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        hard.motorEsquerdaTras.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        hard.motorDireitaTras.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        runtime.reset();
+
+        //Espera o botão start na DS
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            /*
+             * =============================================================================
+             *                            FIELD ORIENTED DRIVE
+             * =============================================================================
+             */
+
+            //Variáveis gamepad
+            drive = -gamepad1.left_stick_y;
+            turn = gamepad1.left_stick_x * 1.5;
+            giro = gamepad1.right_stick_x;
+
+            //Calculo para field oriented
+            processamentoGame(drive, turn);
+
+            //Valores para movimentação com mechanum (lados espelhados)
+            //Motor Esquerda Frente;
+            poder[0] = drive + turn + giro;
+            //Motor Esquerda trás;
+            poder[1] = drive - turn + giro;
+            //Motor Direita Frente;
+            poder[2] = drive - turn - giro;
+            //Motor Direita trás;
+            poder[3] = drive + turn - giro;
+
+            //Verificar se algum valor é maior que 1
+            if (Math.abs(poder[0]) > 1 || Math.abs(poder[1]) > 1
+                    || Math.abs(poder[2]) > 1 || Math.abs(poder[3]) > 1) {
+
+                //Achar o maior valor
+                double max;
+                max = Math.max(Math.abs(poder[0]), Math.abs(poder[1]));
+                max = Math.max(Math.abs(poder[2]), max);
+                max = Math.max(Math.abs(poder[3]), max);
+
+                //Não ultrapassar +/-1 (proporção);
+                poder[0] /= max;
+                poder[1] /= max;
+                poder[2] /= max;
+                poder[3] /= max;
+            }
+
+            //Metodo setPower que manda força para os motores.
+            hard.motorEsquerda.setPower(poder[0]);
+            hard.motorEsquerdaTras.setPower(poder[1]);
+            hard.motorDireita.setPower(poder[2]);
+            hard.motorDireitaTras.setPower(poder[3]);
+
+            /*
+             * =============================================================================
+             *                                MOTORES
+             * =============================================================================
+             */
+
+
+
+            /*
+             * =============================================================================
+             *                                SERVOS
+             * =============================================================================
+             */
+
+        }
+    }
+
+    private void processamentoGame ( double driveP, double turnP){
+        double angle = gyroCalculate() * pi / 180;
+        drive = driveP * Math.cos(angle) - turnP * Math.sin(angle);
+        turn = driveP * Math.sin(angle) + turnP * Math.cos(angle);
+    }
+
+    private double gyroCalculate () {
+        angles = HardwareClassProtipagem.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        return angles.firstAngle;
+    }
+}
