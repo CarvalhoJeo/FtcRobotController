9e0fc132b6a3994466f016595411ded8c3d69174
lcarvalho14391 <carvgamer@gmail.com>
2021-09-21 22:12:43 -0300

    Prototyping control finished

--- TeamCode/src/main/java/prototipagemControl/TeleOpPrototipagem.java
+++ TeamCode/src/main/java/prototipagemControl/TeleOpPrototipagem.java
@@ -23,6 +23,7 @@ public class TeleOpPrototipagem extends LinearOpMode{
     //Respectivamente eixos do gamepad y, x, x outro analógico
     double drive, turn, giro;
     double pi = 3.1415926;
+    double power = 0;
 
     //Vetor para potência do motor
     private final double[] poder = new double[4];
@@ -96,17 +97,96 @@ public class TeleOpPrototipagem extends LinearOpMode{
 
             /*
              * =============================================================================
+             *                          CONTROLE DE VELOCIDADE
+             * =============================================================================
+             */
+
+            while (power < 1){
+                if (gamepad1.dpad_left){
+                    power =- 0.1;
+                }else if(gamepad1.dpad_right){
+                    power =+ 0.1;
+                }
+                if (power > 1){
+                    power = 1;
+                }
+            }
+            
+            /*
+             * =============================================================================
              *                                MOTORES
              * =============================================================================
              */
 
-
+            while (!gamepad1.dpad_up){
+                if(gamepad1.a){
+                    hard.motorP0.setPower(power);
+                }else if(gamepad1.b){
+                    hard.motorP1.setPower(power);
+                }else if(gamepad1.x){
+                    hard.motorP2.setPower(power);
+                }else if(gamepad1.y){
+                    hard.motorP3.setPower(power);
+                }
+            }
 
             /*
              * =============================================================================
              *                                SERVOS
              * =============================================================================
              */
+
+            //Programação para servo limitado
+
+            while (gamepad1.dpad_up){
+                if(gamepad1.a){
+                    hard.servoP0.setPosition(1);
+                }else if(gamepad1.b){
+                    hard.servoP1.setPosition(1);
+                }else if(gamepad1.x){
+                    hard.servoP2.setPosition(1);
+                }else if(gamepad1.y){
+                    hard.servoP3.setPosition(1);
+                }else if(gamepad1.left_bumper){
+                    hard.servoP4.setPosition(1);
+                }else if(gamepad1.right_bumper){
+                    hard.servoP4.setPosition(1);
+                }else {
+
+                    hard.servoP0.setPosition(0);
+                    hard.servoP1.setPosition(0);
+                    hard.servoP2.setPosition(0);
+                    hard.servoP3.setPosition(0);
+                    hard.servoP4.setPosition(0);
+                    hard.servoP5.setPosition(0);
+                }
+            }
+
+            //Programação para servo continuo
+
+            /*
+            while (gamepad1.dpad_up){
+                if(gamepad1.a){
+                    hard.servoP0.setPower(power);
+                }else if(gamepad1.b){
+                    hard.servoP1.setPower(power);
+                }else if(gamepad1.x){
+                    hard.servoP2.setPower(power);
+                }else if(gamepad1.y){
+                    hard.servoP3.setPower(power);
+                }else if(gamepad1.left_bumper){
+                    hard.servoP4.setPower(power);
+                }else if(gamepad1.right_bumper){
+                    hard.servoP5.setPower(power);
+                }else {
+                    hard.servoP0.setPower(0);
+                    hard.servoP1.setPower(0);
+                    hard.servoP2.setPower(0);
+                    hard.servoP3.setPower(0);
+                    hard.servoP4.setPower(0);
+                    hard.servoP5.setPower(0);
+                }
+            }*/
 
         }
     }
